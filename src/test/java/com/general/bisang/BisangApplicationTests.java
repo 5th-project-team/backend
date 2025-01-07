cd515c3e1314753e1c1a02b1c5d7f4ecb05d0369
PaikMyeongGyu <qoraudrb123@gmail.com>
2025-01-07 20:20:47 +0900

    chore: 프로젝트 의존성 설정

---
+++ src/test/java/com/general/bisang/BisangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.general.bisang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BisangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
